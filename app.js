fd7d1e8e34e438e75983ef24fbff3479ac7e7ed8
Vinicius Melo <viniciusmelo554@gmail.com>
2024-06-22 17:03:29 -0300

    cabeçalho com treino do dia

--- app.js
+++ app.js
@@ -6,30 +6,43 @@ import {
   get,
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAgEAeATp9zhWUff-rK2LXDWjmo4gr6fPU",
-  authDomain: "fir-back-test.firebaseapp.com",
-  databaseURL: "https://fir-back-test-default-rtdb.firebaseio.com",
-  projectId: "fir-back-test",
-  storageBucket: "fir-back-test.appspot.com",
-  messagingSenderId: "75117160680",
-  appId: "1:75117160680:web:d399831e99225c79e7415d",
-  measurementId: "G-QRHE0629LX",
-};
+var app, database, databaseRef, banco;
 
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-const databaseRef = ref(database);
+(function () {
+  const firebaseConfig = {
+    apiKey: "AIzaSyAgEAeATp9zhWUff-rK2LXDWjmo4gr6fPU",
+    authDomain: "fir-back-test.firebaseapp.com",
+    databaseURL: "https://fir-back-test-default-rtdb.firebaseio.com",
+    projectId: "fir-back-test",
+    storageBucket: "fir-back-test.appspot.com",
+    messagingSenderId: "75117160680",
+    appId: "1:75117160680:web:d399831e99225c79e7415d",
+    measurementId: "G-QRHE0629LX",
+  };
 
-get(child(databaseRef, `treinos`))
-  .then((snapshot) => {
-    if (snapshot.exists()) {
-      console.log(snapshot.val());
-    } else {
-      console.log("No data available");
-    }
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+  app = initializeApp(firebaseConfig);
+  database = getDatabase(app);
+  databaseRef = ref(database);
 
+  get(child(databaseRef, `treinos/hoje`))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        switch (snapshot.val()) {
+          case 0:
+            document.getElementById("treinoDia").innerHTML = "Peito/Triceps/Ombro(EMPURRAR)"
+            break;
+          case 1:
+            console.log("Puxar");
+            break;
+          case 2:
+            console.log("Pernas");
+            break;
+        }
+      } else {
+        console.log("No data available");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+})();
